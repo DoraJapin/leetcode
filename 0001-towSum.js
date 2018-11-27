3749d4378d78679bf96a8babac67d32dacd37412
邓杰荣 <1970236127@qq.com>
2018-11-27 21:29:06 +0800

    towSum

---
+++ 0001-towSum.js
@@ -0,0 +1,27 @@
+/**
+ *  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+	You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+	Example:
+	Given nums = [2, 7, 11, 15], target = 9,
+	Because nums[0] + nums[1] = 2 + 7 = 9,
+	return [0, 1].
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let consult=[]
+    nums.map((item, index)=> {
+        nums.map((child, i)=>  {
+            if(i !== index) {
+                if(item+child == target) {
+                    consult = [index, i]
+                }
+            }
+        })
+    }) 
+    return consult
+};
